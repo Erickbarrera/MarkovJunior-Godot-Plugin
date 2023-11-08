561124f34d96ac4ebd1b17fd2c845dacd2e1e699
Erick Barrera <me@erickbarrera.com>
2023-11-07 19:56:54 -0600

    Initial commit

---
+++ Scripts/Markov.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+using markov;
+
+public partial class Markov : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+//		Generate.GenerateModel("test");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
